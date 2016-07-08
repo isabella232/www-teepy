a73dab7074f01d30dedd8601fcd1fe73bd42ee0b
Guillaume Ayoub <guillaume.ayoub@kozea.fr>
2016-07-08 17:45:25 +0200

    Fix contacts

--- teepy.py
+++ teepy.py
@@ -1,13 +1,44 @@
 #!/usr/bin/env python
-from flask import Flask, render_template
+from flask import Flask, redirect, render_template, request, url_for
+import mandrill
 
 
 app = Flask(__name__)
+app.config.from_envvar('BACKOFFICE_CONFIG', silent=True)
+
+MANDRILL_KEY = app.config.get('MANDRILL_KEY')
+
 
 @app.route('/')
 @app.route('/<page>')
 def page(page='index'):
     return render_template('{}.html'.format(page), page=page)
+
+
+@app.route('/contact', methods=['POST'])
+def contact():
+    form = request.form
+    message = {
+        'to': [{'email': 'backoffice@lagestiondutierspayant.fr'}],
+        'subject': 'Prise de contact sur le site de ',
+        'from_email': 'backoffice@lagestiondutierspayant.fr'}
+    if 'name' in form:
+        message['html'] = '<br>'.join([
+            'Nom : %s' % form['name'],
+            'Téléphone : %s' % form['phone'],
+            'Email : %s' % form['email'],
+            'Demande : %s ' % form['message']])
+    else:
+        message['html'] = '<br>'.join([
+            'Demande de rappel au téléphone.',
+            'Téléphone : %s' % form['phone']])
+
+    if not app.debug:
+        mandrill.Mandrill(MANDRILL_KEY).send(message=message)
+    else:
+        print(message['html'])
+
+    return redirect(url_for('page', page='contact_confirmation'))
 
 
 if __name__ == '__main__':
