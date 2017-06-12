b675a0f1ac1a1de3178782b73c50980620ec95c5
Guillaume Ayoub <guillaume.ayoub@kozea.fr>
2017-06-12 17:44:03 +0200

    Add a 404 page
    Fix #793.

--- teepy.py
+++ teepy.py
@@ -1,7 +1,9 @@
 #!/usr/bin/env python3
 
+import jinja2
 import mandrill
-from flask import Flask, Response, redirect, render_template, request, url_for
+from flask import (Flask, Response, abort, redirect, render_template, request,
+                   url_for)
 
 app = Flask(__name__)
 app.config.from_envvar('BACKOFFICE_CONFIG', silent=True)
@@ -12,7 +14,10 @@ MANDRILL_KEY = app.config.get('MANDRILL_KEY')
 @app.route('/')
 @app.route('/<page>')
 def page(page='index'):
-    return render_template('{}.html'.format(page), page=page)
+    try:
+        return render_template('{}.html'.format(page), page=page)
+    except jinja2.exceptions.TemplateNotFound:
+        abort(404)
 
 
 @app.route('/robots.txt')
@@ -42,6 +47,11 @@ def contact():
         mandrill.Mandrill(MANDRILL_KEY).messages.send(message=message)
 
     return redirect(url_for('page', page='contact_confirmation'))
+
+
+@app.errorhandler(404)
+def page_not_found(error):
+    return render_template('404.html'), 404
 
 
 if __name__ == '__main__':
